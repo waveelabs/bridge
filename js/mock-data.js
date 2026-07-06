e1081e77d60a1e68f96066ed59912118c2e99c51
fgyarmati <gyarmati.fanny@gmail.com>
2026-07-06 17:29:04 +0200

    alap weboldal
    rad var a logika c:

---
+++ js/mock-data.js
@@ -0,0 +1,46 @@
+// nyinyinyinyi - have fun
+
+const STORAGE_KEY = 'bridge_database';
+
+
+//olvasás
+function getDatabase() {
+    let data = localStorage.getItem(STORAGE_KEY);
+
+    if (data === null) {
+        return null;
+        // nincs még tournament
+    }
+
+    return JSON.parse(data);
+}
+
+//mentés
+
+function saveDatabase(data) {
+    let dataString = JSON.stringify(data);
+    localStorage.setItem(STORAGE_KEY, dataString);
+}
+
+// uj bizbasz (tournament)
+
+function createTournament(name, tableCount, boardCount) {
+    let newTournament = {
+        name: name,
+        tableCount: tableCount,
+        boardCount: boardCount,
+        results: [] //array, amibe majd az eredmények kerülnek
+    };
+
+    saveDatabase(newTournament);
+    return newTournament;
+}
+
+// ez ilyen hard reset, ami nekem nagyon jó, mert
+// egyszerre 1 bizbasszal tudok dealelni, de itt belenyomhatod 
+// az eredményeket valami tartós tárba, idk rád bízom
+
+function clearDatabase() {
+    localStorage.removeItem(STORAGE_KEY);
+}
+
